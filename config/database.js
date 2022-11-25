6dc0dd2a15bd934bff151577e726d826be57043c
unknown <muhammadanugrahp@gmail.com>
2022-11-25 20:34:16 +0700

    fixdatabase

--- config/database.js
+++ config/database.js
@@ -1,8 +1,15 @@
-import sequelize from "sequelize";
+import  Sequelize  from "sequelize";
 
-const db = new sequelize('d88e182dkcfs1b','thfsxqwjfkjklr','8f86a5316f0befd189fbe1f2ecadaa9525c0d7de5f65e0e5f077cf85d3a7737c',{
-    host: "ec2-54-80-123-146.compute-1.amazonaws.com",
-    dialect: "postgres"
-});
+export const db = new Sequelize('d88e182dkcfs1b', 'thfsxqwjfkjklr', '8f86a5316f0befd189fbe1f2ecadaa9525c0d7de5f65e0e5f077cf85d3a7737c', {
+  host: 'ec2-54-80-123-146.compute-1.amazonaws.com',
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+   },
+ },
+})
 
 export default db;
