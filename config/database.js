a09606f450558e5dce8a939dd6e1dfc352dc271c
unknown <muhammadanugrahp@gmail.com>
2022-11-25 20:13:45 +0700

    fixxx

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import sequelize from "sequelize";
 
 const db = new sequelize('d88e182dkcfs1b','thfsxqwjfkjklr','8f86a5316f0befd189fbe1f2ecadaa9525c0d7de5f65e0e5f077cf85d3a7737c',{
     host: "ec2-54-80-123-146.compute-1.amazonaws.com",
-    dialect: "mysql"
+    dialect: "postgreesql"
 });
 
 export default db;
